4bcd3037392912da8e88a838f5d980f9db0173ac
Cecilia Lindgren <cecilia.lindgren@student.tabyenskilda.se>
2021-01-12 12:58:13 +0100

    test

---
+++ script.js
@@ -0,0 +1,8 @@
+<!-- göm/visa rubrik -->
+
+function visarubrik() {
+document.getElementById('h1').style.visibility = 'visible';
+}
+function gömrubrik() {
+document.getElementById('h1').style.visibility = 'hidden '
+}
